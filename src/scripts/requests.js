982e9804e8388c2979997889642bcb2442ae2f00
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2022-12-21 09:12:34 -0300

    renderizou-se os pokemons e realizou-se a estilização dos cards

--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -25,10 +25,27 @@ async function renderizaPokemons() {
   pokemonList.forEach(pokemon => {
     const pokedexNumber = pokemon.url.slice(34, -1);
 
-    console.log(pokedexNumber);
+    let pokemons = createCards(pokemon, pokedexNumber);
+
+    ul.appendChild(pokemons);
   });
 
   
+}
+
+function createCards (pokemon, pokedex) {
+  let li = document.createElement('li');
+
+  let pokemonImage = document.createElement('img');
+  pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedex}.png`;
+  pokemonImage.alt = pokemon.name;
+
+  let pokemonName = document.createElement('h3');
+  pokemonName.innerText = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
+
+  li.append(pokemonImage, pokemonName);
+
+  return li;
 }
 
 renderizaPokemons()
