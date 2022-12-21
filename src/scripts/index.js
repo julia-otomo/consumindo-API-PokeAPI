362f8a0d9001a8389703c788fb140908be8d79ef
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2022-12-21 10:58:35 -0300

    ajustes no arquivo index.js e request.js (as funções de redenrização foram adicionadas no ducumento index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,29 @@
-// async function renderizaPokemons() {
-//     const pokemons = await pokemonList;
-
-//     console.log(pokemons);
-// }
-
-// renderizaPokemons()
+function renderizaPokemons(array) {
+    const ul = document.querySelector('#pokemon_list');
+  
+    array.forEach(pokemon => {
+      const pokedexNumber = pokemon.url.slice(34, -1);
+  
+      let pokemons = createCards(pokemon, pokedexNumber);
+  
+      ul.appendChild(pokemons);
+    });
+  
+    
+  }
+  
+  function createCards (pokemon, pokedex) {
+    let li = document.createElement('li');
+  
+    let pokemonImage = document.createElement('img');
+    pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedex}.png`;
+    pokemonImage.alt = pokemon.name;
+  
+    let pokemonName = document.createElement('h3');
+    pokemonName.innerText = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
+  
+    li.append(pokemonImage, pokemonName);
+  
+    return li;
+  }
+  
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -8,44 +8,15 @@ async function consomePokeAPI() {
     }
   })
     .then ((response) => response.json())
+    .then((response) => {
+      loading.classList.add('hidden');
+      return renderizaPokemons(response.results)
+    }
+   )
     .catch (() => console.log('Não foi possível buscar informações nessa API'))
-
-    loading.classList.add('hidden');
     
     return pokemonsDaApi.results;
 }
 
 consomePokeAPI();
 
-async function renderizaPokemons() {
-  const ul = document.querySelector('#pokemon_list');
-
-  const pokemonList = await consomePokeAPI();
-
-  pokemonList.forEach(pokemon => {
-    const pokedexNumber = pokemon.url.slice(34, -1);
-
-    let pokemons = createCards(pokemon, pokedexNumber);
-
-    ul.appendChild(pokemons);
-  });
-
-  
-}
-
-function createCards (pokemon, pokedex) {
-  let li = document.createElement('li');
-
-  let pokemonImage = document.createElement('img');
-  pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedex}.png`;
-  pokemonImage.alt = pokemon.name;
-
-  let pokemonName = document.createElement('h3');
-  pokemonName.innerText = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
-
-  li.append(pokemonImage, pokemonName);
-
-  return li;
-}
-
-renderizaPokemons()
